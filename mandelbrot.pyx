ebb0519cf039e9c2af1d44d27f8afc1c012ee79e
Tamura Dai <kirinode0@gmail.com>
2022-12-11 20:42:29 +0900

    Fix for performance up with parallel processing.

--- mandelbrot.pyx
+++ mandelbrot.pyx
@@ -1,12 +1,17 @@
+#distutils: extra_compile_args = -fopenmp
+#distutils: extra_link_args = -fopenmp
+from threading import Thread
+from cython.parallel import *
+
 # cython: boundscheck=False
 # cytohn: wrapround=False
 
 import numpy as np
 
-cdef inline double norm2(double complex z):
+cdef inline double norm2(double complex z) nogil:
     return z.real * z.real + z.imag * z.imag
 
-cdef int escape (double complex z, double complex c, double z_max, int n_max):
+cdef int escape (double complex z, double complex c, double z_max, int n_max) nogil:
     cdef:
         int i = 0
         double z_max2 = z_max * z_max
@@ -26,7 +31,7 @@ def mandelbrot(int resolution, double bound=1.2, double z_max=2.0, int n_max=500
 
     counts = np.zeros((resolution+1,resolution+1), dtype=np.int32)
 
-    for i in range(resolution+1):
+    for i in prange(resolution+1, nogil=True, schedule="static", chunksize=1):
         imag = bound - i * step
         for j in range(resolution+1):
             real = -bound + j * step - 0.5
